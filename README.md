e755b8509a6a2b7d6c4815692f919dfa7fefa216
Goutam Tamvada <xvzcf@users.noreply.github.com>
2022-08-05 16:00:22 +0200

    Adding X25519+Kyber512 capability in TLS 1.3

--- README.md
+++ README.md
@@ -1,11 +1,13 @@
-# Firefox-Go server KEMTLS Demo
+# Firefox PQ Demos
+
+0. Set `DO_KEMTLS` in `build.sh` according to whether you want Firefox to do KEMTLS with Kyber512 or TLS 1.3 with X25519+Kyber512
 
 1. Run `build.sh`. Keep the default `mozilla-unified` as the source directory, and when asked to choose which version of Firefox to build, choose option 2: `Firefox for Desktop`.
 
-2. Start the Go KEMTLS server by running: `./cf-pq-kemtls/bin/go run server.go`
+2. Start Firefox by running `./mozilla-unified/mach run`
 
-3. Start Firefox by running `./mozilla-unified/mach run`
+3. In Firefox, go to `about:config` and set `network.http.http2.enforce-tls-profile` to `false`.
 
-4. In Firefox, go to `about:config` and set `network.http.http2.enforce-tls-profile` to `false`.
+4. (If doing KEMTLS) Start the Go KEMTLS server by running: `./cf-go/bin/go run server.go`, and then load `https://localhost:4433` in Firefox.
 
-5. Load `https://localhost:4433` in Firefox.
+5. Load `https://pq.cloudflareresearch.com`.
--- build.sh
+++ build.sh
@@ -3,10 +3,20 @@ set -e
 
 ROOTDIR=$(pwd)
 
+DO_KEMTLS=false
+
+if [ "${DO_KEMTLS}" = true ] ; then
+    NSS_PATCH=nss-kemtls-patch
+    FIREFOX_PATCH=firefox-patch
+else
+    NSS_PATCH=nss-hybrid-pq-patch
+    FIREFOX_PATCH=firefox-hybrid-pq-patch
+fi
+
 # Clean up from previous run, if any
 rm -f bootstrap.py
 rm -rf mozilla-unified
-rm -rf cf-pq-kemtls
+rm -rf cf-go
 
 # Build Firefox
 sudo apt-get install curl python3 python3-dev python3-pip mercurial git
@@ -15,11 +25,14 @@ curl https://hg.mozilla.org/mozilla-central/raw-file/default/python/mozboot/bin/
 python3 bootstrap.py --no-system-changes
 
 hg update -R mozilla-unified -r 698364:5a3f8ede374b
-git apply --directory=mozilla-unified/security/nss nss-kemtls-patch
-git apply --directory=mozilla-unified firefox-patch
+
+git apply --directory=mozilla-unified/security/nss "${NSS_PATCH}"
+git apply --directory=mozilla-unified "${FIREFOX_PATCH}"
 ./mozilla-unified/mach build
 
 # Build CF's fork of Go containing the KEMTLS implementation
-cd "${ROOTDIR}"
-git clone --branch cf-pq-kemtls --depth 1 https://github.com/cloudflare/go cf-pq-kemtls
-cd cf-pq-kemtls/src && ./make.bash
+if [ "${DO_KEMTLS}" = true ] ; then
+    cd "${ROOTDIR}"
+    git clone --branch cf-pq-kemtls --depth 1 https://github.com/cloudflare/go cf-go
+    cd cf-go/src && ./make.bash
+fi
